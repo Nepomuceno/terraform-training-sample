63499716adb87f1294b78ee04121fc6ed25e1a74
Gabriel Nepomuceno <gabriel.nepomuceno@microsoft.com>
2020-01-03 03:11:20 +0000

    adding backend

--- 1-exemplo/main.tf
+++ 1-exemplo/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "ganepomutfstate"
+    container_name       = "tfstate"
+    key                  = "tfdemo-take.tfstate"
+    resource_group_name  = "terraform"
+  }
+}
 provider "azurerm" {
   version = "=1.36.1"
 }
